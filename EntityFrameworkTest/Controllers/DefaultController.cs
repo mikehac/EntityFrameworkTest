ba74df6c70d1f6997b6c070d871d4a4ea9c37d7f
Mike Hatskevich <mike.hatskevich@gmail.com>
2019-03-15 14:12:31 +0200

    Adding all project files
    Adding all project files

---
+++ EntityFrameworkTest/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using EntityFrameworkTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace EntityFrameworkTest.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            roomhikeEntities db = new roomhikeEntities();
+            var firstUser = db.AspNetUsers.FirstOrDefault();
+            if (!System.Web.Security.Roles.Enabled)
+            {
+                System.Web.Security.Roles.Enabled = true;
+            }
+
+            string guestRole = "Guest";
+            var guestRoleExists = Roles.RoleExists(guestRole);
+            if (!guestRoleExists)
+            {
+                Roles.CreateRole(guestRole);
+            }
+            Roles.AddUserToRole(firstUser.UserName, guestRole);
+            return View();
+        }
+    }
+}
---
+++ EntityFrameworkWS/Service1.cs
@@ -0,0 +1,47 @@
+﻿using EntityFrameworkWS.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace EntityFrameworkWS
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+#if (DEBUG)
+            Debugger.Launch();
+#endif
+            roomhikeEntities db = new roomhikeEntities();
+            var firstUser = db.AspNetUsers.FirstOrDefault();
+            string guestRole = "Guest";
+            var guestRoleExists = Roles.RoleExists(guestRole);
+            if (!guestRoleExists)
+            {
+                Roles.CreateRole(guestRole);
+            }
+            if (Roles.IsUserInRole(firstUser.UserName,guestRole))
+            {
+                Roles.RemoveUserFromRole(firstUser.UserName, guestRole);
+            }
+            Roles.AddUserToRole(firstUser.UserName, guestRole);
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
